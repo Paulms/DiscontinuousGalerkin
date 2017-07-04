605908e3b5d7a4337aded5e191f6c7ae9b569dba
Paul Mendez <pmendez@ing-mat.udec.cl>
2017-07-04 12:25:22 -0400

    Initial structure of residual function

--- examples/advection.jl
+++ examples/advection.jl
@@ -25,5 +25,27 @@ u = hcat(zeros(u0[:,1]),u0,zeros(u0[:,1]))
 #build inverse of mass matrix
 M_inv = get_local_inv_mass_matrix(mybasis, mesh)
 
-#Reconstruct first u
+#Reconstruct u in finite space
 uₕ = mybasis.φₕ*u
+
+#Reconstruct u on faces
+uₛ = mybasis.ψₕ*u
+
+"Calculates residual for DG method
+  residual = M_inv*(Q+F)
+         where M_inv is the inverse mass matrix
+              Q is the edge fluxes
+              F is the interior flux"
+function residual{T}(basis::Basis{T})
+  F = uₕ
+  # Integrate interior fluxes
+  F = A_mul_B!(F, basis.φₕ.*basis.weights, F)
+
+  # Evaluate edge fluxes
+  q = zeros(T,size(uₛ,2)-1)
+  for i = 1:(size(uₛ,2)-1)
+    ul = @view uₛ[2,i]; ur = @view uₛ[1,i+1]
+    q[i] = advection_solver(ul,ur)
+  end
+
+end
